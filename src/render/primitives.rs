6c8684e5d8cba677d7e89896228a98f435e0df73
Andreas Longva <andreas.b.longva@gmail.com>
2016-07-27 17:02:36 +0200

    Refactor build_triangle_renderable

--- src/render/primitives.rs
+++ src/render/primitives.rs
@@ -3,6 +3,22 @@ use glium;
 use render::*;
 use cgmath::Point3;
 
+pub fn build_renderable<F>(display: &F,
+    vertices: &[RenderVertex],
+    triangle_indices: &[u32])
+    -> SceneRenderable where F: Facade {
+        let vertex_buffer = glium::VertexBuffer::new(display, vertices).unwrap();
+        let index_buffer = glium::IndexBuffer::new(display,
+            glium::index::PrimitiveType::TrianglesList,
+            triangle_indices).unwrap();
+
+        use std::rc::Rc;
+        SceneRenderable {
+            vertices: Rc::new(vertex_buffer),
+            indices: Rc::new(index_buffer)
+        }
+}
+
 pub fn build_triangle_renderable<F>(display: &F, a: Point3<f32>, b: Point3<f32>, c: Point3<f32>)
     -> SceneRenderable where F: Facade {
 
@@ -11,17 +27,8 @@ pub fn build_triangle_renderable<F>(display: &F, a: Point3<f32>, b: Point3<f32>,
     let c = RenderVertex::from(c);
 
     let vertices = vec!(a, b, c);
-
-    let vertex_buffer = glium::VertexBuffer::new(display, &vertices).unwrap();
-    let indices = glium::IndexBuffer::new(display,
-        glium::index::PrimitiveType::TrianglesList,
-        &[0, 1, 2]).unwrap();
-
-    use std::rc::Rc;
-    SceneRenderable {
-        vertices: Rc::new(vertex_buffer),
-        indices: Rc::new(indices)
-    }
+    let indices = [0, 1, 2];
+    build_renderable(display, &vertices, &indices)
 }
 
 pub fn build_tetrahedron_renderable<F>(display: &F) -> SceneRenderable where F: Facade {
