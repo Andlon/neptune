1d5437547e7e9284fa6c0e945f5987cd5cd49977
Andreas Longva <andreas.b.longva@gmail.com>
2016-08-07 00:09:50 +0200

    Fix error in Blinn-Phong shader

--- src/render/scene_renderer.rs
+++ src/render/scene_renderer.rs
@@ -71,7 +71,7 @@ impl SceneRenderer {
 
             uniform vec3 light_direction;
 
-            const vec3 ambient_color = vec3(0.2, 0.0, 0.0);
+            const vec3 ambient_color = vec3(0.05, 0.0, 0.0);
             const vec3 diffuse_color = vec3(0.6, 0.0, 0.0);
             const vec3 specular_color = vec3(1.0, 1.0, 1.0);
 
@@ -79,7 +79,7 @@ impl SceneRenderer {
                 float diffuse = max(dot(normalize(vertex_normal), normalize(light_direction)), 0.0);
 
                 vec3 camera_dir = normalize(-vertex_position);
-                vec3 half_direction = normalize(normalize(light_direction) + camera_dir);
+                vec3 half_direction = normalize(light_direction + camera_dir);
                 float specular = pow(max(dot(half_direction, normalize(vertex_normal)), 0.0), 16.0);
 
                 color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
