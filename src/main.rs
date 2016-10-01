5b7c33e4397086f013c5c70e994f9a03d4aeb6f9
Andreas Longva <andreas.b.longva@gmail.com>
2016-10-01 15:42:34 +0200

    Rewrite scene0 to use builder pattern for easy modification

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,16 @@ use engine::Engine;
 
 struct Initializer;
 
+use entity::EntityBlueprint;
 use entity::blueprints;
 use camera::Camera;
 use render::Color;
 use engine::{SceneBlueprint, SceneInitializer};
 
-impl engine::SceneInitializer for Initializer {
+use cgmath::{Point3, Vector3, EuclideanSpace, Zero, Quaternion};
+use geometry::{Sphere, Cuboid};
+
+impl SceneInitializer for Initializer {
     fn create_scene(&self, index: usize) -> Option<SceneBlueprint> {
         match index {
             0 => Some(self.create_scene0()),
@@ -37,99 +41,212 @@ impl engine::SceneInitializer for Initializer {
     }
 }
 
+struct SphereObject {
+    center: Point3<f64>,
+    velocity: Vector3<f64>,
+    radius: f64,
+    mass: f64,
+    color: Color,
+    subdivisions: u32
+}
+
+struct CuboidObject {
+    center: Point3<f64>,
+    orientation: Quaternion<f64>,
+    velocity: Vector3<f64>,
+    half_size: Vector3<f64>,
+    mass: f64,
+    color: Color,
+}
+
+fn main() {
+    let initializer = Initializer;
+    let mut engine = Engine::new();
+    engine.run(&initializer);
+}
+
+impl Default for SphereObject {
+    fn default() -> Self {
+        let gray = Color::rgb(0.5, 0.5, 0.5);
+        SphereObject {
+            center: Point3::origin(),
+            velocity: Vector3::zero(),
+            radius: 1.0,
+            mass: 1.0,
+            color: gray,
+            subdivisions: 3
+        }
+    }
+}
+
+impl SphereObject {
+    fn center(mut self, center: Point3<f64>) -> SphereObject {
+        self.center = center;
+        self
+    }
+
+    fn velocity(mut self, velocity: Vector3<f64>) -> SphereObject {
+        self.velocity = velocity;
+        self
+    }
+
+    fn radius(mut self, radius: f64) -> SphereObject {
+        self.radius = radius;
+        self
+    }
+
+    fn mass(mut self, mass: f64) -> SphereObject {
+        self.mass = mass;
+        self
+    }
+
+    fn color(mut self, color: Color) -> SphereObject {
+        self.color = color;
+        self
+    }
+
+    fn subdivisions(mut self, subdivisions: u32) -> SphereObject {
+        self.subdivisions = subdivisions;
+        self
+    }
+
+    fn create_blueprint(self) -> EntityBlueprint {
+        let sphere = Sphere {
+            center: self.center,
+            radius: self.radius
+        };
+        let mut blueprint = blueprints::sphere(sphere, self.mass, self.subdivisions);
+        blueprint.renderable.as_mut().unwrap().color = self.color;
+        blueprint.physics.as_mut().unwrap().velocity = self.velocity;
+        blueprint
+    }
+}
+
+impl Default for CuboidObject {
+    fn default() -> Self {
+        let gray = Color::rgb(0.5, 0.5, 0.5);
+        CuboidObject {
+            center: Point3::origin(),
+            velocity: Vector3::zero(),
+            half_size: Vector3::new(0.5, 0.5, 0.5),
+            orientation: Quaternion::new(1.0, 0.0, 0.0, 0.0),
+            mass: 1.0,
+            color: gray,
+        }
+    }
+}
+
+impl CuboidObject {
+    fn center(mut self, center: Point3<f64>) -> Self {
+        self.center = center;
+        self
+    }
+
+    fn velocity(mut self, velocity: Vector3<f64>) -> Self {
+        self.velocity = velocity;
+        self
+    }
+
+    fn half_size(mut self, half_size: Vector3<f64>) -> Self {
+        self.half_size = half_size;
+        self
+    }
+
+    fn orientation(mut self, orientation: Quaternion<f64>) -> Self {
+        self.orientation = orientation;
+        self
+    }
+
+    fn mass(mut self, mass: f64) -> Self {
+        self.mass = mass;
+        self
+    }
+
+    fn color(mut self, color: Color) -> Self {
+        self.color = color;
+        self
+    }
+
+    fn create_blueprint(self) -> EntityBlueprint {
+        let cuboid = Cuboid {
+            center: self.center,
+            half_size: self.half_size,
+            rotation: self.orientation
+        };
+
+        let mut blueprint = blueprints::cuboid(cuboid, self.mass);
+        blueprint.renderable.as_mut().unwrap().color = self.color;
+        blueprint.physics.as_mut().unwrap().position = self.center;
+        blueprint.physics.as_mut().unwrap().velocity = self.velocity;
+        blueprint
+    }
+}
+
 impl Initializer {
     fn create_scene0(&self) -> SceneBlueprint {
-        use cgmath::{Point3, Vector3, Quaternion, EuclideanSpace};
-        use geometry::{Sphere, Cuboid};
+        use cgmath::{Quaternion};
 
         let camera = Camera::look_in(Point3::new(40.0, 0.0, 0.0), -Vector3::unit_x(), Vector3::unit_z())
                             .unwrap();
-
-        let mut blueprints = Vec::new();
 
         let blue = Color::rgb(0.0, 0.0, 1.0);
         let red = Color::rgb(1.0, 0.0, 0.0);
         let green = Color::rgb(0.0, 1.0, 0.0);
         let graybrown = Color::rgb(205.0 / 255.0, 133.0 / 255.0 ,63.0/255.0);
 
-        {
-            let sphere = Sphere {
-                center: Point3::origin(),
-                radius: 5.0
-            };
-            let mut blueprint = blueprints::sphere(sphere, 1e11, 4);
-            blueprint.renderable.as_mut().unwrap().color = blue;
-            blueprints.push(blueprint);
-        }
+        let blueprints = vec![
+            SphereObject::default()
+                         .radius(5.0)
+                         .mass(1e11)
+                         .color(blue)
+                         .subdivisions(4)
+                         .create_blueprint(),
 
-        {
-            let sphere = Sphere {
-                center: Point3::new(0.0, 15.0, 15.0),
-                radius: 1.0
-            };
-            let mut blueprint = blueprints::sphere(sphere, 1.0, 3);
-            blueprint.renderable.as_mut().unwrap().color = graybrown;
-            blueprint.physics.as_mut().unwrap().velocity = Vector3::new(0.0, 2.5, 0.0);
-            blueprints.push(blueprint);
-        }
+            SphereObject::default()
+                         .center(Point3::new(0.0, 15.0, 15.0))
+                         .velocity(Vector3::new(0.0, 2.5, 0.0))
+                         .radius(1.0)
+                         .mass(1.0)
+                         .color(graybrown)
+                         .create_blueprint(),
 
-        {
-            let sphere = Sphere {
-                center: Point3::new(5.0, 15.0, 0.0),
-                radius: 1.0
-            };
-            let mut blueprint = blueprints::sphere(sphere, 1.0, 3);
-            blueprint.renderable.as_mut().unwrap().color = red;
-            blueprint.physics.as_mut().unwrap().velocity = Vector3::new(0.0, 0.0, 1.5);
-            blueprints.push(blueprint);
-        }
+            SphereObject::default()
+                         .center(Point3::new(5.0, 15.0, 0.0))
+                         .velocity(Vector3::new(0.0, 0.0, 1.5))
+                         .radius(1.0)
+                         .mass(1.0)
+                         .color(red)
+                         .create_blueprint(),
 
-        {
-            let sphere = Sphere {
-                center: Point3::new(0.0, 15.0, -5.0),
-                radius: 1.0
-            };
-            let mut blueprint = blueprints::sphere(sphere, 1.0, 3);
-            blueprint.renderable.as_mut().unwrap().color = red;
-            blueprint.physics.as_mut().unwrap().velocity = Vector3::new(0.0, 1.0, 2.0);
-            blueprints.push(blueprint);
-        }
+            SphereObject::default()
+                         .center(Point3::new(0.0, 15.0, -5.0))
+                         .velocity(Vector3::new(0.0, 1.0, 2.0))
+                         .radius(1.0)
+                         .mass(1.0)
+                         .color(red)
+                         .create_blueprint(),
 
-        {
-            let sphere = Sphere {
-                center: Point3::new(0.0, 15.0, 0.0),
-                radius: 1.0
-            };
-            let mut blueprint = blueprints::sphere(sphere, 1.0, 3);
-            blueprint.renderable.as_mut().unwrap().color = red;
-            blueprint.physics.as_mut().unwrap().velocity = Vector3::new(0.0, -2.0, 0.0);
-            blueprints.push(blueprint);
-        }
+            SphereObject::default()
+                         .center(Point3::new(0.0, 15.0, 0.0))
+                         .velocity(Vector3::new(0.0, -2.0, 0.0))
+                         .radius(1.0)
+                         .mass(1.0)
+                         .color(red)
+                         .create_blueprint(),
 
-        {
-            let cuboid = Cuboid {
-                center: Point3::new(0.0, -40.0, 0.0),
-                half_size: Vector3::new(5.0, 5.0, 10.0),
-                rotation: Quaternion::new(1.0, 0.0, 0.0, 0.0)
-            };
-
-            let mut blueprint = blueprints::cuboid(cuboid, 0.2);
-            blueprint.renderable.as_mut().unwrap().color = green;
-            blueprint.physics.as_mut().unwrap().position = Point3::new(0.0, -40.0, 0.0);
-            blueprints.push(blueprint);
-        }
+            CuboidObject::default()
+                         .center(Point3::new(0.0, -40.0, 0.0))
+                         .velocity(Vector3::zero())
+                         .half_size(Vector3::new(5.0, 5.0, 10.0))
+                         .orientation(Quaternion::new(1.0, 0.0, 0.0, 0.0))
+                         .mass(0.2)
+                         .color(green)
+                         .create_blueprint()
+        ];
 
         SceneBlueprint {
             blueprints: blueprints,
             camera: camera
         }
     }
-}
-
-
-
-fn main() {
-    let initializer = Initializer;
-    let mut engine = Engine::new();
-    engine.run(&initializer);
 }
